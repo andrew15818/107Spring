884e850fda541e8926f698182043e06b7230ffb2
andrew15818 <andreseeponce@gmail.com>
2019-04-08 23:16:17 +0000

    Initial Commit

--- CompOrg/Lab1/alu.v
+++ CompOrg/Lab1/alu.v
@@ -25,7 +25,7 @@ module alu(
            src1,          // 32 bits source 1          (input)
            src2,          // 32 bits source 2          (input)
            ALU_control,   // 4 bits ALU control input  (input)
-		 //bonus_control, // 3 bits bonus control input(input) 
+		   bonus_control, // 3 bits bonus control input(input) 
            result,        // 32 bits result            (output)
            zero,          // 1 bit when the output is 0, zero must be set (output)
            cout,          // 1 bit carry out           (output)
@@ -37,17 +37,109 @@ input           rst_n;
 input  [32-1:0] src1;
 input  [32-1:0] src2;
 input   [4-1:0] ALU_control;
-//input   [3-1:0] bonus_control; 
+input   [3-1:0] bonus_control; 
 
 output [32-1:0] result;
 output          zero;
 output          cout;
 output          overflow;
 
-reg    [32-1:0] result;
 reg             zero;
-reg             cout;
 reg             overflow;
 
+//Our own regs
+reg             A_invert; 
+reg             B_invert; 
+reg             src1_bu;//Back up for when we want to use src1 for other operations(like subtration) 
+reg             src2_bu;//Back up for when we want to use src2 for other operations(like subtration) 
+reg             cin;//This can be used for twos complement
+wire     [32-1:0] mulCout; //check why this has to be wire and not register
+reg     [1:0]   subOp;
 
+
+
+alu_top alut_top_0(
+    .src1(src1[0]),
+    .src2(src2[0]),
+    .less(result[31]),//I think this is what it is
+    .equal(1'b0),//TODO change this to actually do something
+    .lsb(1'b1),
+    .A_invert(A_invert),
+    .B_invert(B_invert),
+    .cin(cin),
+    .comp(bonus_control),
+    .operation(subOp),
+    .result(result[0]),
+    .cout(mulCout[0])
+    );
+genvar ind;
+generate
+    for(ind =1;ind<32;ind= ind+1) begin:for_i
+        alu_top alut_top_i(
+        .src1(src1[ind]),
+        .src2(src2[ind]),
+        .less(1'b0),
+        .equal(1'b0),//TODO change this to actually do something
+        .lsb(1'b0),
+        .A_invert(A_invert),
+        .B_invert(B_invert),
+        .cin(mulCout[ind-1]),
+        .comp(bonus_control),
+        .operation(subOp),
+        .result(result[ind]),
+        .cout(mulCout[ind])
+    );
+end
+
+endgenerate
+
+always @(*) begin
+    case(ALU_control)
+        4'b0000 : begin//AND
+            A_invert = 1'b0;
+            B_invert = 1'b0;
+            cin = 1'b0;
+            subOp = 0;
+        end
+        4'b0001 : begin//OR
+            A_invert = 1'b0;
+            B_invert = 1'b0;
+            cin = 1'b0;
+            subOp = 1;
+        end
+        4'b0010 : begin//Addition(Unsigned (I think))
+            A_invert = 1'b0;
+            B_invert = 1'b0;
+            cin =1'b0;
+            subOp = 2;
+            end
+        4'b0110 : begin //Subctract(Unsinged)
+            A_invert <= 1'b0;
+            B_invert <= 1'b1;
+            cin <=1'b1;
+            subOp = 2;//Basically adding A and B's Complement
+            end
+        4'b1100 : begin //Nor
+            A_invert = 1'b1;
+            B_invert = 1'b1;
+            cin = 1'b0;
+            subOp = 0;//A'B' = A NOR B
+        end
+        4'b1101 : begin //Nand
+            A_invert = 1'b1;
+            B_invert = 1'b1;
+            cin = 1'b0;
+            subOp = 0;//a'+b' = a nand b
+        end
+        4'b0111 : begin//Set Less than
+            A_invert = 1'b0;
+            B_invert = 1'b1;
+            cin = 1'b1;
+            subOp = 2;
+            //TODO ahorita solo modifica el lsb del result
+            //lo que podriamos hacer para dejar todo en 0 es hacerle and con 00..01 per
+            //en el siguiente "computation cycle"
+        end
+    endcase
+end
 endmodule
--- CompOrg/Lab1/alu_top.v
+++ CompOrg/Lab1/alu_top.v
@@ -1,53 +1,71 @@
 `timescale 1ns/1ps
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:15:11 03/21/2019
-// Design Name: 
-// Module Name:    alu_top 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 module alu_top(
-               src1,       //1 bit source 1 (input)
-               src2,       //1 bit source 2 (input)
-               less,       //1 bit less     (input)
-               A_invert,   //1 bit A_invert (input)
-               B_invert,   //1 bit B_invert (input)
-               cin,        //1 bit carry in (input)
-               operation,  //operation      (input)
-               result,     //1 bit result   (output)
-               cout,       //1 bit carry out(output)
-               );
+	src1,       //1 bit source 1 (input)
+	src2,       //1 bit source 2 (input)
+	less,       //1 bit less     (input)
+    equal,
+    lsb,
+	A_invert,   //1 bit A_invert (input)
+	B_invert,   //1 bit B_invert (input)
+	cin,        //1 bit carry in (input)
+    comp,       //3 bit comp     (input)
+	operation,  //operation      (input)
+	result,     //1 bit result   (output)
+	cout       //1 bit carry out(output)
+);
 
-input         src1;
-input         src2;
-input         less;
-input         A_invert;
-input         B_invert;
-input         cin;
-input [2-1:0] operation;
+input			src1;//lets treat it as A
+input			src2;//lets treat it as B
+input			less;
+input           equal; 
+input           lsb;
+input			A_invert;
+input			B_invert;
+input			cin;
+input   [2:0]   comp;
+input	[2-1:0]	operation;
 
-output        result;
-output        cout;
+output results;
+output cout;
 
-reg           result;
+reg result;
+reg cout;
 
-always@( * )
+wire a;
+wire b;
+assign a = (A_invert) ? ~src1 : src1;
+assign b = (B_invert) ? ~src2 : src2;
+always@(*)
 begin
-
+    cout <=0;
+    case(operation)
+        0: result <= a & b;//ANDing
+        1: result <= a | b;//ORing(inclusive)
+        2:begin//Addition
+            result <= (a ^ b) ^ cin; //XOR for addition with cin
+            cout <= (cin & (a | b)) | a & b;//TODO might want to change that a|b to a^b
+        end
+        3:begin//Comparison
+            case(comp)
+                3'b000: begin
+                    if (lsb & less) begin
+                        result =1'b1;//slt(Set less than)
+                    end
+                    else begin
+                            result = 1'b0;
+                    end
+                end
+                //3'b001://Sgt(Set great than)
+                //3'b010://Sle(set less equal)
+                //3'b011://Sge(Set great equal)
+                //3'b110://Seq(Set Equal)
+                //3'b100://Sne(Set not equal)
+            endcase
+        end
+        default: begin
+        end
+    endcase
 end
 
 endmodule

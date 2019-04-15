b5ab41dc332141846c61981f7cf949695a2f0101
andrew15818 <andreseeponce@gmail.com>
2019-04-15 20:19:42 +0000

    not going to split into two files

--- DSOOP/homework/hw2/Link_List.h
+++ DSOOP/homework/hw2/Link_List.h
@@ -44,57 +44,54 @@ private:
 	int size;
 	Int_Node<T> *head, *tail;								// pointer to the first and the last element of Link_List
 };
-/* 
- * For a linked list, the heaad pointer is at the left in your drawing, therefore head->pre should be null, same as 
- * tail->next
-#  */
 /*Initial constructor only creates head,tail ptrs, sets size to zero, values
  * and other nodes are created elsewhere as needed.*/
-/*
 template<typename T>
 Link_List<T>::Link_List(){
-	head = tail = nullptr;
+	head = tail = NULL;
 	size=0;
 }
-*set a tmp node equal to the header and set the corresponding node in the list
+/*set a tmp node equal to the header and set the corresponding node in the list
  * with the same value*/
-/*
+
 template<typename T>
 Link_List<T>::Link_List(const Link_List<T>& ll){
 	Int_Node<T> *tmp  = new Int_Node<T>; 		/*node object we will use for traversing the list*/
 	tmp = ll.head;
-	head=tail=nullptr;
+	head=tail=NULL;
 	size =0;
-	while(tmp !=  nullptr){	
+	while(tmp !=  NULL){	
 		Int_Node<T> *data= new Int_Node<T>;
 		data->value  = tmp->value;
 		if(size==0){
-			data->pre =data->next = nullptr;	/*creating first node, setting equal to head, tail */
+			data->pre =data->next = NULL;	/*creating first node, setting equal to head, tail */
 			head = tail = data;
 		}
 		else{
 			data->pre   = tail; 				/*adding a new node, shifting accordingly*/
-			data->next  = nullptr;
+			data->next  = NULL;
 			tail->next = data;
 			tail = data;	
 		}
 		tmp=tmp->next;
 		size++;
-	}	
-	//cout<<"size after copying "<<size<<endl;
+	}		
 	delete tmp;
 }
 /*destructor deletes every node*/
+/*set two tmp variables, one for traversing list and another one to delete*/
 template<typename T>
 Link_List<T>::~Link_List(){	
 	Int_Node<T> *tmp = new Int_Node<T>;
 	tmp = head;
 	while(tmp!= NULL){
-		delete tmp;
+		Int_Node<T> *tmpo = tmp;
 		tmp = tmp->next;	
+		delete tmpo;
 		size--;
 	}
 }
+/*Return size of List*/
 template<typename T>
 int Link_List<T>::getSize() const {
 	return size;
@@ -104,17 +101,16 @@ int Link_List<T>::getSize() const {
 template<typename U>
 istream& operator>>(istream& is, Link_List<U>& ll){
 	Int_Node<U> *data= new Int_Node<U>; 
+	is>>data->value;
 	/*If there are no nodes, create a new one with value*/
-	if( ll.head	== nullptr && ll.tail == nullptr && ll.size ==0){
-		is>>data->value;
-		data->pre=data->next = nullptr;
+	if( ll.head	== NULL && ll.tail == NULL && ll.size ==0){	
+		data->pre=data->next = NULL;
 		ll.head = ll.tail =data;
 		ll.size++;
 		return is;
 	}
-		is>>data->value;
 		data->pre = ll.tail;
-		data->next = nullptr;
+		data->next = NULL;
 		ll.tail->next = data;
 		ll.tail = data;		
 		ll.size++;
@@ -123,7 +119,7 @@ istream& operator>>(istream& is, Link_List<U>& ll){
 template<typename U>
 ostream& operator<<(ostream& os, const Link_List<U>& ll){	
 	Int_Node<U> *tmp = ll.head;
-	while(tmp!=nullptr){
+	while(tmp!=NULL){
 		os<<tmp->value<<" ";
 		tmp=tmp->next;	
 	}
@@ -137,13 +133,13 @@ const Link_List<T>& Link_List<T>::operator=(const Link_List& ll){
 		Int_Node<T> *data;
 		if(size==0){
 			data->value = tmp->value;
-			data->next=data->prev = nullptr;	
+			data->next=data->prev = NULL;	
 			head = tail =data;
 			continue;
 		}
 		data->value = tmp->value;	
 		data->prev = tail;
-		data->next = nullptr;
+		data->next = NULL;
 		tail = data;
 		tmp= tmp->next;	
 	}
@@ -162,7 +158,6 @@ bool Link_List<T>::operator==(const Link_List<T>& ll)const{
 		tmp = tmp->next;
 		current = current->next;
 	}
-
 	return true;
 }
 /*To return the value at the index, we have to traverse the entire list up
@@ -201,7 +196,7 @@ bool Link_List<T>::insert_node(T value){
 
 	if(size ==0){
 		head = data;			/*If the list is empty, create a new first node with value*/
-		data->pre=nullptr;	
+		data->pre=NULL;	
 
 	}
 	else{	
@@ -209,7 +204,7 @@ bool Link_List<T>::insert_node(T value){
 		tail->next= data;
 	}
 	tail=data;
-	data->next=nullptr;
+	data->next=NULL;
 	size++;
 	return true;
 }
@@ -221,7 +216,7 @@ bool Link_List<T>::delete_node(){
 		return false;	
 	}	
 	Int_Node<T> *tmp = tail;
-	tail->pre->next = nullptr;
+	tail->pre->next = NULL;
 	tail = tail->pre;
 
 	delete tmp;
@@ -270,5 +265,5 @@ bool Link_List<T>::delete_node(int index){
 	}
 	return true;
 }
-*/
+
 #endif // LINK_LIST
